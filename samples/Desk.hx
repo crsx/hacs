5693cb4fd49ff5be3a76970557521572e51fb500
Kristoffer Rose <krisrose@crsx.org>
2015-02-24 01:06:59 -0500

    Desk example included.

---
+++ samples/Desk.hx
@@ -0,0 +1,36 @@
+module edu.nyu.cs.cc.Desk {
+
+  // Syntax.
+
+  token NUM | [0-9]+ ;
+
+  sort E
+    | ⟦ ⟨NUM⟩ ⟧@5
+    | sugar ⟦ ( ⟨E#⟩ ) ⟧@5 → #
+
+    | ⟦ ⟨E@2⟩ / ⟨E@3⟩ ⟧@2
+    | ⟦ ⟨E@2⟩ * ⟨E@3⟩ ⟧@2
+
+    | ⟦ ⟨E@1⟩ - ⟨E@2⟩ ⟧@1
+    | ⟦ ⟨E@1⟩ + ⟨E@2⟩ ⟧@1
+    ;
+
+  sort Computed | scheme Eval(E) ;
+  Eval(⟦ ⟨NUM#⟩ ⟧) → ⟦ $# ⟧ ;
+
+  Eval(⟦ ⟨E#1⟩ + ⟨E#2⟩ ⟧) →  Plus(Eval(#1), Eval(#2)) ;
+  | scheme Plus(Computed, Computed) ;
+  Plus(#1, #2) → ⟦ #1 + #2 ⟧ ;
+
+  Eval(⟦ ⟨E#1⟩ - ⟨E#2⟩ ⟧) →  Minus(Eval(#1), Eval(#2)) ;
+  | scheme Minus(Computed, Computed) ;
+  Minus(#1, #2) → ⟦ #1 - #2 ⟧ ;
+  
+  Eval(⟦ ⟨E#1⟩ * ⟨E#2⟩ ⟧) →  Times(Eval(#1), Eval(#2)) ;
+  | scheme Times(Computed, Computed) ;
+  Times(#1, #2) → ⟦ #1 * #2 ⟧ ;
+
+  Eval(⟦ ⟨E#1⟩ / ⟨E#2⟩ ⟧) →  Divide(Eval(#1), Eval(#2)) ;
+  | scheme Divide(Computed, Computed) ;
+  Divide(#1, #2) → ⟦ #1 / #2 ⟧ ;
+}
