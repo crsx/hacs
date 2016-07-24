779eabed9bb3c38b7e9069164eeba323e2bcc9b8
Kristoffer Rose <krisrose@debian.org>
2016-07-24 13:01:49 -0400

    Fixed raw abstraction parser generation.

--- samples/CPS.hx
+++ samples/CPS.hx
@@ -1,20 +1,16 @@
-// samples/CPS.hx: Continuation-passing for 2-level λ-calculus in -*-hacs-*-
+// Convert λ-calculus with Call-by-Value semantics to CPS in -*-hacs-*-
 module org.crsx.hacs.samples.CPS {
-
-  // Tokens.
   space [ \t\n] ;
   token ID | [a-z] [_0-9]* ;  // single-letter identifiers
 
-  // λ Calculus Grammar.
-  main sort E
+  main sort E   // λ calculus grammar.
   |  ⟦  λ ⟨ID binds x⟩ . ⟨E[x as E]⟩ ⟧ | ⟦ ⟨E@1⟩ ⟨E@2⟩ ⟧@1
-  |  symbol ⟦ ⟨ID⟩ ⟧@2 | sugar ⟦ ( ⟨E#⟩ ) ⟧@2→# ;
+  |  symbol ⟦⟨ID⟩⟧@2  | sugar ⟦ ( ⟨E#⟩ ) ⟧@2→# ;
 
-  // One-pass CBV CPS.
-  | scheme CPS(E) ;
-  CPS(#) → ⟦ λk.{⟨E#⟩ | m.k m} ⟧ ;
+  | scheme CPS(E) ;   // one-pass Call-by-Value CPS.
+  CPS(#) →  ⟦ λk.{⟨E#⟩ | m.k m} ⟧ ;
   | scheme ⟦ {⟨E⟩ | ⟨ID binds m⟩ . ⟨E[m as E]⟩} ⟧@2 ;
-  ⟦ {v | m.⟨E#F[m]⟩} ⟧ →  #F[v] ;
-  ⟦ {λx.⟨E#[x]⟩ | m.⟨E#F[m]⟩} ⟧→#F[E⟦ λx.λk.{⟨E#[x]⟩ | m.k m} ⟧] ;
-  ⟦ {⟨E#0⟩ ⟨E#1⟩ | m.⟨E#F[m]⟩} ⟧→⟦{⟨E#0⟩ | m.{⟨E#1⟩ | n.m n (λa.⟨E#F[a]⟩)}}⟧;
+  ⟦ {v | m.⟨E#F[m]⟩} ⟧   →  #F[v] ;
+  ⟦ {λx.⟨E#[x]⟩ | m.⟨E#F[m]⟩} ⟧   →  #F[E⟦ λx.λk.{⟨E#[x]⟩ | m.k m} ⟧] ;
+  ⟦ {⟨E#0⟩ ⟨E#1⟩ | m.⟨E#F[m]⟩} ⟧   →  ⟦{⟨E#0⟩ | m.{⟨E#1⟩ | n.m n (λa.⟨E#F[a]⟩)}}⟧;
 }
--- samples/Lambda.hx
+++ samples/Lambda.hx
@@ -1,10 +1,10 @@
 module org.crsx.hacs.samples.Lambda {
-  space [ \t\n] ;
+
   token ID | [a-z] [_0-9]* ;
 
   main sort T
-  | ⟦λ ⟨ID binds x⟩ . ⟨T[x as T]⟩⟧		   // abstraction
-  | scheme ⟦⟨T@1⟩ ⟨T@2⟩⟧@1			// application
+  | ⟦λ ⟨ID binds x⟩ . ⟨T[x as T]⟩⟧		   // abstraction is data
+  | scheme ⟦⟨T@1⟩ ⟨T@2⟩⟧@1			// application is executable
   | sugar ⟦(⟨T#⟩)⟧@2 →  # ;
 
   ⟦(λ x . ⟨T#1[T⟦x⟧]⟩) ⟨T#2⟩⟧ →  #1[T#2] ;
